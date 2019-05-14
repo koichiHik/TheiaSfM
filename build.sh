66650e03d88bc37b9f6c4377e6b8f59c0cad3ecc
Koichi Hikawa <rkoichi2001@gmail.com>
2019-05-14 22:34:29 +0900

    Compiled
    Change-Id: I6b489fa78214f952a151af290afd03a8a20cd694

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ROOT_3RD=~/workspace/3rdParty
+CMAKE_BUILD_TYPE=Debug
+ROCKSDB_HOME=${ROOT_3RD}/rock_sdb/install/
+
+cd ..
+
+if [ ! -e ./build ]; then
+  mkdir build
+fi
+cd build
+
+cmake \
+  -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+  -D CMAKE_INSTALL_PREFIX="../install" \
+  -D ROCKSDB_HOME=${ROCKSDB_HOME} \
+  ../TheiaSfM
+
+make -j32
+
+cd ../TheiaSfM
